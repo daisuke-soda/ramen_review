57d5aabf60b86a9eeeadbc7d39e1e556de67daec
daisuke-soda <68390854+daisuke-soda@users.noreply.github.com>
2021-03-08 15:48:51 +0900

    seeds.rb追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_074242) do
+ActiveRecord::Schema.define(version: 2021_03_08_064408) do
+
+  create_table "ramen_shops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
